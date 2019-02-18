b987836a9ffda589d2eaecaa2384a290813a0758
root <root@localhost.localdomain>
2019-02-17 21:53:29 -0800

    ARP req/resp implemented, generically

--- NetworkAttacks/network_scanner.py
+++ NetworkAttacks/network_scanner.py
@@ -7,6 +7,7 @@
 # Details: Code that uses module netifaces to access and get back your mac/ip address if an interface is specified.
 # Possible that a machine might have more than one address. Thus we loop through ni.interfaces() and access the AF_LINK/
 # AF_INET method which is a list of dictionaries to grab all the neccessary addresses.
+# THEN the ARP request/response begins on all networks of the subnet
 
 
 import scapy.all as scapy
@@ -18,7 +19,23 @@ import optparse
 
 
 def scan(ip):
-    scapy.arping(ip)
+        print("\nARP protocol begins.\n")
+        arp_request = scapy.ARP(pdst = ip) #ARP request from my IP searching for our parameter IP
+        ether_obj = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") #broadcast destination ff:ff....:ff must be used
+        # for all broadcasts that way it reaches all stations
+        
+        packet = ether_obj/arp_request  #Ethernet part to send the destination broadcast, arp part for all ip
+        # / is used to append objects
+        _dict = {}
+        answered = scapy.srp(packet, timeout=2, verbose=False)[0] #Sends packet with our own customized ether part <ether_obj>
+
+        for pckt in answered:
+                _dict[pckt[1].psrc] = pckt[1].hwsrc
+        
+        for k in _dict.keys():
+                print("IP Address: ", k)
+                print("Mac Address: ", _dict[k])
+                print("--------------------------------------------")   
 
 def get_AllmacAddr():
     for i in ni.interfaces(): 
@@ -51,12 +68,14 @@ def main():
         get_AllipAddr()
     else:
         interface=options.interface
-        print("\n[+] IP Address \n")
+        print("\n[+] My IP Address \n")
         i = getIP()
         print(i)
-        print("\n[+] Mac Address \n")
+        print("\n[+] My Mac Address \n")
         m = getMac()
         print(m)
+        print("\n\n")
+        scan(str(i)+'/24')
 
 
 if __name__ == '__main__':
